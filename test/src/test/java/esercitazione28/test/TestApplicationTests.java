92bd5fa4f11ab98fe4a9ea93f65653ea6a0711cc
costa <m.manole@dstech.it>
2025-02-25 12:49:45 +0100

    Commit iniziale

---
+++ test/src/test/java/esercitazione28/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package esercitazione28.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
